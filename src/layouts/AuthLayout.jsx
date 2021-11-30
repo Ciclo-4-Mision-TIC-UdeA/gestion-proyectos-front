f021fbb888c450eb66c53855eb8c6c38f9e88e96
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-11-30 17:39:35 +0000

    agregar despliegue heroku

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -6,7 +6,7 @@ const AuthLayout = () => {
     <div className='flex flex-col md:flex-row flex-no-wrap h-screen'>
       <div className='flex w-full h-full'>
         <div className='w-full h-full  overflow-y-scroll'>
-          Layout de Autenticacion
+          Layout de Autenticacion Desplegado
           <Outlet />
         </div>
       </div>
