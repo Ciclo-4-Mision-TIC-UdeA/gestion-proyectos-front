c3bad87ee966933f0e088cf57c3d6671a41f3d44
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-13 16:52:35 +0000

    subir imagenes

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import useFormData from 'hooks/useFormData';
+import { uploadFormData } from 'utils/uploadFormData';
 
 const Page2 = () => {
-  return <div>Pagina 2</div>;
+  const { formData, updateFormData, form } = useFormData(null);
+  const submitForm = async (e) => {
+    e.preventDefault();
+    console.log('beforeUpload', formData);
+    const uploadedFD = await uploadFormData(formData);
+    console.log('after', uploadedFD);
+  };
+  return (
+    <div>
+      <form ref={form} onChange={updateFormData} onSubmit={submitForm}>
+        <input type='file' name='test_cloudinary' />
+        <button type='submit'>Ok</button>
+      </form>
+    </div>
+  );
 };
 
 export default Page2;
