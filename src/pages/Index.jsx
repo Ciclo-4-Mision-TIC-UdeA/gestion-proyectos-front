39f0516c9302ca2f2df9550ed84aafaa92d83a4e
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-14 19:55:43 +0000

    add deploy stage

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Index = () => (
   <div>
-    <div className='bg-green-300 h-96'>Index page</div>
+    <div className='bg-green-300 h-96'>Index page test deploy</div>
     <div className='bg-green-300 h-96'>Index page</div>
     <div className='bg-green-300 h-96'>Index page</div>
     <div className='bg-green-300 h-96'>Index page</div>
