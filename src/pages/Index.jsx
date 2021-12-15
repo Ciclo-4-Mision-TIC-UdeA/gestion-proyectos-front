20ff7ae97ca2e3f2b8d9ef6c669031facf106ab3
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-15 17:47:53 +0000

    agregar pagina de dev

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Index = () => (
   <div>
     <div className='bg-green-300 h-96'>
-      Esta es una pagina que esta en estado de testing
+      Esta es una pagina que esta en estado de desarrollo
     </div>
     <div className='bg-green-300 h-96'>Index page</div>
     <div className='bg-green-300 h-96'>Index page</div>
