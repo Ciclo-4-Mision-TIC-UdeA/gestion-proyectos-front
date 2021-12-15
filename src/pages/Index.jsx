8dc877f4fd82827d6bb73265adf39913e7f5b9fe
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-15 17:37:41 +0000

    agregar workflow de deploy

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Index = () => (
   <div>
-    <div className='bg-green-300 h-96'>Index page test deploy</div>
+    <div className='bg-green-300 h-96'>
+      Index page hola estoy desplegando desde gh actions
+    </div>
     <div className='bg-green-300 h-96'>Index page</div>
     <div className='bg-green-300 h-96'>Index page</div>
     <div className='bg-green-300 h-96'>Index page</div>
