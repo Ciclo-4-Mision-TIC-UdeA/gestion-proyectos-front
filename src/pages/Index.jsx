d832dded36fad43f2885c84dc4e9fd023a0cc443
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-15 17:54:11 +0000

    nuevo test de dev

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Index = () => (
   <div>
     <div className='bg-green-300 h-96'>
-      Esta es una pagina que esta en estado de desarrollo
+      Esta es una pagina que esta en estado de desarrollo, test 2
     </div>
     <div className='bg-green-300 h-96'>Index page</div>
     <div className='bg-green-300 h-96'>Index page</div>
