41e4858fb5331946e7f286ab2d9fcfad051aa997
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-15 17:47:23 +0000

    agregar prueba en testing

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Index = () => (
   <div>
     <div className='bg-green-300 h-96'>
-      Index page hola estoy desplegando desde gh actions
+      Esta es una pagina que esta en estado de testing
     </div>
     <div className='bg-green-300 h-96'>Index page</div>
     <div className='bg-green-300 h-96'>Index page</div>
